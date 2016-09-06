199438f544959c503ee023f9b7a6dc952fd2d2f0
Konstantin Kowalski <kkostya@ubuntu.members.linode.com>
2016-09-06 20:42:25 +0000

    Adding backend

--- header.js
+++ header.js
@@ -1,14 +1,28 @@
-$(function() {
+
+function fixNavWidth() {
+   var navWidth = 0;
+   $('.nav-item').each(function() {
+      //css('width');
+      var width = $(this).outerWidth(true);
+      //console.log(typeof width);
+      navWidth += width;
+   });
+
+   //console.log(navWidth);
+   navWidth += 1;
+   $('#top-nav').css('width', navWidth + 'px');
+}
+
+$(document).ready(function() {
    $('#family-cape-logo').click(function() {
       window.location = 'http://familycape.com/';
    });
 
-   var navWidth = 0;
+   //TODO: fixme and use static width
+   setTimeout(fixNavWidth, 50);
+
    $('.nav-item').each(function() {
-      var width = $(this).width();//css('width');
-      //console.log(width);
-      navWidth += width;
+      if ($(this).text().trim() == 'Home')
+         $(this).addClass('nav-item-active');
    });
-   console.log(navWidth);
-   $('#top-nav').css('width', navWidth + 'px');
 });
