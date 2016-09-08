4107bb30e87886902926c9b42c11f0bab3f578c4
Konstantin Kowalski <konstantin@businesscape.com>
2016-09-08 18:04:10 +0000

    Maxing slideshow look better.

--- static-nginx/index.js
+++ static-nginx/index.js
@@ -1,13 +1,4 @@
 $(function() {
-   function setSlideArrowColor(c) {
-      $('.prev, .next').css('background-color', c);
-
-   }
-   $('.slideshow-container').hover(function() {
-      setSlideArrowColor('rgba(0, 0, 0, 0.8)');
-   }, function() {
-      setSlideArrowColor('white');
-   });
 
 });
 
--- static-nginx/slideshow.js
+++ static-nginx/slideshow.js
@@ -2,6 +2,24 @@ var slideIndex = 1;
 
 $(function() {
    showSlides(slideIndex);
+   $('.mySlides').hide();
+   $('.captionText').hide();
+
+   function setSlideArrowColor(c) {
+      $('.prev-slide, .next-slide').css('background-color', c);
+   }
+   setSlideArrowColor('rgba(0, 0, 0, 0.8)');
+
+   function setSlideArrowOpacity(n) {
+      $('.prev-slide, .next-slide').css('opacity', n);
+   }
+   $('.slideshow-container').hover(function() {
+      setSlideArrowOpacity(1);
+   }, function() {
+      //setSlideArrowColor('white');
+      setSlideArrowOpacity(0);
+   });
+
 });
 
 function plusSlides(n) {
@@ -21,19 +39,62 @@ function autoRotate(time) {
    setTimeout(rotator, time);
 }
 
+function showSlide(n) {
+   var slides = $('.mySlides');
+   var dots = $('.dot');
+
+   if (n >= slides.length)
+      n = 0;
+   if (n < 0)
+      n = slides.length - 1;
+}
+
+//var active = false;
 function showSlides(n) {
+   /*if (active)
+      return;
+   active = true;*/
    var i;
    var slides = document.getElementsByClassName("mySlides");
    var dots = document.getElementsByClassName("dot");
-   if (n > slides.length) {slideIndex = 1}
-   if (n < 1) {slideIndex = slides.length}
-   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+   if (n > slides.length)
+      slideIndex = 1;
+   if (n < 1)
+      slideIndex = slides.length;
+
+   for (var i = 0; i < slides.length; i++) {
+      //slides[i].style.display = "none";
+      //$(slides[i]).hide();
+      /*var cssV = {
+         opacity:       0,
+         transition:    'opacity 1s'
+      };
+      $('.active-slide').css(cssV).slideUp(1000);*/
+      //$('.active-slide').fadeOut('slow').slideUp('slow');
+      $('.active-slide').fadeOut(500);
+      $('.active-slide').removeClass('active-slide');
    }
-   for (i = 0; i < dots.length; i++) {
+   for (var i = 0; i < dots.length; i++)
       dots[i].className = dots[i].className.replace(" active", "");
-   }
-   slides[slideIndex-1].style.display = "block";
+   var slide = slides[slideIndex-1];
+   //$(slide).slideDown();
+   /*var cssV = {
+      opacity:    1,
+      transition: 'opacity 1s'
+   };*/
+   //$(slide).fadeIn('slow').slideDown('slow');
+   /*setTimeout(function() {
+      $(slide).css(cssV).slideDown(1000);
+      $(slide).addClass('active-slide');
+   }, 500);*/
+   $(slide).addClass('active-slide');
+   setTimeout(function() {
+      $(slide).fadeIn(500);
+      //active = false;
+   }, 600);
+   //slides[slideIndex-1].style.display = "block";
    dots[slideIndex-1].className += " active";
 }
+
+
 
