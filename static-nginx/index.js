75fa4f2682ecf9734638415dd9c4753429455f4b
Konstantin Kowalski <konstantin@businesscape.com>
2016-09-09 17:39:33 +0000

    Adding form stuff.

--- static-nginx/index.js
+++ static-nginx/index.js
@@ -1,5 +1,17 @@
-$(function() {
 
+var vidFixerTimeout = 600;
+function vidFixer() {
+   var vidWidth = $('#bottom-video').width();
+   console.log(vidWidth);
+   var vidHeight = Math.round(vidWidth / 1.7);
+   $('#bottom-video').css('height', vidHeight);
+   $('#vid-n-text-placeholder').css('height', $('#vid-n-text').height() + 'px');
+   setTimeout(vidFixer, vidFixerTimeout);
+}
+
+$(function() {
+   $('#vid-n-text-placeholder').css('height', $('#vid-n-text').height() + 'px');
+   vidFixer();
 });
 
 
